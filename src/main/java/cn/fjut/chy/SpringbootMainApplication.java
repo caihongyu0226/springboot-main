bff314e344f5c7cb23ea87b51836061713d9dcd9
caihongyu0226 <823696721@qq.com>
2020-07-08 19:10:34 +0800

    Initial commit

---
+++ src/main/java/cn/fjut/chy/SpringbootMainApplication.java
@@ -0,0 +1,13 @@
+package cn.fjut.chy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/fjut/chy/SpringbootMainApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fjut.chy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
