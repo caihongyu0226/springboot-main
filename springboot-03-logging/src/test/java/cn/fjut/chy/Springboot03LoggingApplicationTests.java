ae25bf7481f38b3c2084a86d850265ac3a7e9ce0
caihongyu0226 <823696721@qq.com>
2020-07-08 19:17:07 +0800

    springboot-03-logging

---
+++ springboot-03-logging/src/test/java/cn/fjut/chy/Springboot03LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fjut.chy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
